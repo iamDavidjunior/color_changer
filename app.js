685660121ed247e27ac681862c0c65499c989bd4
iamDavidjunior <iamdavidjunior@outlook.com>
2022-02-14 14:25:06 +0200

    minor changes

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const button = document.getElementById('button');
-const color = document.querySelector('.color');
-const box = document.querySelector('.box'); 
+const button = document.getElementById("button");
 
-button.addEventListener('click', () => {
-    let hexColor = '#';
-    for(let i = 0; i<6; i++){
-        hexColor += hex[getRandomNumber()]
-    }
-    color.textContent = hexColor;
-    box.style.backgroundColor = hexColor
-})
+const color = document.querySelector(".color");
+
+const box = document.querySelector(".box");
+
+button.addEventListener("click", () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  color.textContent = hexColor;
+  box.style.backgroundColor = hexColor;
+});
 
 const getRandomNumber = () => {
-    return Math.floor(Math.random() * hex.length)
-}
+  return Math.floor(Math.random() * hex.length);
+};
